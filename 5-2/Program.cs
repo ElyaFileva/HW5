46772f1901b68b4044ba9c83240223d21579b3b1
Elya Fileva <filevaelya@mail.ru>
2023-02-26 19:25:54 +0300

    Добавлена задача 5-2

---
+++ 5-2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+
+int[] array = FillArray(3);
+
+Console.WriteLine($"В массиве: [{string.Join(" ", array)}] сумма элементов на нечетной позиции {GetSumOddPositionByArray(array)}");
+
+int[] FillArray(int size)
+{
+  int[] result = new int[size];
+  Random rnd = new Random();
+  for (int i = 0; i < result.Length; i++)
+  {
+    result[i] = rnd.Next(-100, 101);
+  }
+  return result;
+}
+
+int GetSumOddPositionByArray(int[] array)
+{
+  int result = 0;
+  for (int i = 1; i < array.Length; i += 2)
+  {
+    result += array[i];
+  }
+  return result;
+}
