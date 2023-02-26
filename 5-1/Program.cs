f340baf6e78a045a8ba0cf848c49af6bd90b1eec
Elya Fileva <filevaelya@mail.ru>
2023-02-26 19:24:49 +0300

    Добавлена задача 5-1

---
+++ 5-1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+int[] array = FillArray(10);
+
+Console.WriteLine($"В массиве: [{string.Join(" ", array)}] четных элементов {GetCountEvenNumbersByArray(array)}");
+
+int[] FillArray(int size)
+{
+  int[] result = new int[size];
+  Random rnd = new Random();
+  for (int i = 0; i < result.Length; i++)
+  {
+    result[i] = rnd.Next(100, 1000);
+  }
+  return result;
+}
+
+int GetCountEvenNumbersByArray(int[] array)
+{
+  int result = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i] = array[i] % 10;
+    if (array[i] % 2 == 0 || array[i] == 0)
+      result++;
+  }
+  return result;
+}
